26d7a57f1c8f59869a25826f569470ad3be54cbb
hanneyaline <162329097+hanneyaline@users.noreply.github.com>
2024-04-16 11:07:58 -0300

    Add files via upload

---
+++ animal.py
@@ -0,0 +1,13 @@
+class Animal:
+    def __init__(self, nome, idade, especie):
+        self.nome = nome
+        self.idade = idade
+        self.especie = especie
+    def emetir(self):
+        print("o animal", self.nome, "da especie", self.especie,"esta emetindo som")
+    def informacao(self):
+        print(f"o animal{self.nome}da especie {self.especie}tem {self.idade} anos de idade")
+if __name__=="__main__":
+    animal= Animal("fido", "5", "cachorro")
+    animal.emitir()
+    animal.informacao()
